96b44bb231973813e4156b1c0ab61407dda34380
DominikGovorc <domi.govorc@gmail.com>
2021-02-23 09:43:50 +0100

    V19

---
+++ Vj19 dg/Assets/MoveScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    public float speed;
+    float moveVertical;
+    float moveHorizontal;
+    public Rigidbody2D rb;
+    private void Update() {
+        moveHorizontal = Input.GetAxisRaw ("Horizontal");
+        moveVertical = Input.GetAxisRaw ("Vertical");
+    }
+    private void FixedUpdate()
+{  
+   rb.velocity = new Vector2(moveHorizontal * speed, moveVertical * speed);
+}
+}
